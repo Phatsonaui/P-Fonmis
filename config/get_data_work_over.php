180f7bca5fa119b6d6c6a093fa91bfd75a7aaddf
Phatsonaui <phatsonaui@gmail.com>
2025-09-03 10:56:11 +0700

    Refactor device data retrieval and display logic
    Reworked get_data_work_over.php to fetch and display device information based on POSTed device IDs and status ('in' or 'out'). The script now generates an HTML table with device details and relevant dates, replacing the previous year-based fiscal data logic.

--- config/get_data_work_over.php
+++ config/get_data_work_over.php
@@ -1,29 +1,72 @@
 <?php
-include_once("../function/fun_verify.php");
-if (isUserVerified_fon()) {
-  // รับค่าปีที่ส่งมาผ่าน POST
-  include("../../class/class.db.php");
+include("../../class/class.db.php");
+require_once("../class/chgdatethai.php");
 
-  $selectedYear = isset($_POST['year']) ? $_POST['year'] : '2023';;
-
-  // ดำเนินการดึงข้อมูลหรือประมวลผลตามต้องการ
-  // ตัวอย่างเท่านี้จะนับจำนวนเรื่องทั้งหมดในปีที่เลือก
-  $total_over = 0;
-
-  if ($selectedYear != '') {
-    // echo "if empty";
-    $drow = 0;
-    $db3 = new Database('nurse');
-    $db3->Table = "WL_fiscalYear";
-    $db3->Where = "where fiscal_year='$selectedYear' order by fiscal_year";
-    $user3 = $db3->Select();
-    foreach ($user3 as $values3 => $data3) {
-      $total_over = $data3['fiscal_money'];
-    }
-  }
-
-  // ส่งข้อมูลกลับไปยังเว็บไซต์หลักเป็นผลลัพธ์ที่ต้องการแสดง
-  echo number_format($total_over) . ' บาท';
-} else {
-  echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"2;URL=ctrl_logout.php\">";
+$deviceIDs  = isset($_POST['devices']) ? $_POST['devices'] : "-";
+$status  = isset($_POST['status']) ? $_POST['status'] : "-";
+$idList = implode("','", array_map('addslashes', $deviceIDs));
+$col_name = "";
+$condition = "";
+$date_device = "";
+if ($status == "in") {
+  $col_name = "วันที่รับสินค้า";
+  $condition = "WHERE device_ID IN ('$idList') ORDER BY device_ID ASC";
+} elseif ($status == "out") {
+  $col_name = "วันที่จำหน่าย";
+  $condition = "LEFT JOIN DV_OutStock ON DV_Data.device_ID = DV_OutStock.device_id where DV_OutStock.device_id IN ('$idList')";
 }
+?>
+<div class="col-md-12">
+  <table class="table" id="dtBasicExample">
+    <thead class="Table_header_nu">
+      <tr>
+        <th>#</th>
+        <th>เลขครุภัณฑ์</th>
+        <th width="60%">ชื่อครุภัณฑ์</th>
+        <th width="15%">
+          <?php
+          echo $col_name;
+          ?>
+        </th>
+      </tr>
+    </thead>
+    <tbody class="Table_body_nu">
+      <?php
+      $drow = 0;
+      $db3 = new Database('nurse');
+      $db3->Table = "DV_Data";
+      $db3->Where = "$condition";
+      $user3 = $db3->Select();
+      foreach ($user3 as $values3 => $data3) {
+        $drow++; ?>
+        <tr>
+          <td class="text-center"><?php echo $drow; ?></td>
+          <?php
+          if ($data3['device_Number'] == "") {
+            echo "<td class='text-center'>-</td>";
+          } else { ?>
+            <td><?php echo $data3['device_Number']; ?></td>
+          <?php } ?>
+          <td><span><?php echo $data3['device_Name']; ?></span></td>
+
+          <td style="font-size: 10px;">
+            <?php
+            if ($status == "in") {
+              $date_device =  DateThai($data3['device_DateInput']);
+            } elseif ($status == "out") {
+              $date_device =  DateThai($data3['OutStock_date']);
+            }
+            if ($date_device == "") {
+              echo "-";
+            } else {
+              echo $date_device;
+            }
+            ?>
+          </td>
+        </tr>
+      <?php
+      } ?>
+
+    </tbody>
+  </table>
+</div>
